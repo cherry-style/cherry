881748512cd59fb722ac9bf112bbc459e3087b5a
cherry-style <i@cherry.style>
2020-07-05 16:50:55 +0900

    feat: add simple landing animations

--- apps/cherry.style/client/pages/index.tsx
+++ apps/cherry.style/client/pages/index.tsx
@@ -1,11 +1,17 @@
+import { motion } from 'framer-motion';
 import { NextPage } from 'next';
 import React from 'react';
 
 export const Index: NextPage = () => {
   return (
-    <a href="https://www.notion.so/blossoms/05d20fef8d1041ab942fea4f87367ceb">
+    <motion.a
+      href="https://www.notion.so/blossoms/05d20fef8d1041ab942fea4f87367ceb"
+      initial={{ y: 5, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ delay: 0.5 }}
+    >
       Cherry
-    </a>
+    </motion.a>
   );
 };
 
